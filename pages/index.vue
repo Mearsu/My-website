155550b84484f493abd1f87036342b092d394a1a
Mates Vrba <matesvrba@protonmail.com>
2022-07-30 20:49:06 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+      <h1>Hello world</h1>
+      <p>{{ count }}</p>
+      <button @click="increment"> click me</button>
+      <div v-for="d in data">
+         {{ d }}
+      </div>
+</template>
+
+<script setup>
+const layout = "default";
+const count = ref(0);
+const increment = () => count.value++;
+let data = ["idk"];
+//data = useFetch("https://api.nuxtjs.dev/posts")
+</script>
