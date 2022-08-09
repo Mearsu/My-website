0252fb33fa727ce9b2a0058daf1ddf514b1a0c3c
Mates Vrba <matesvrba@protonmail.com>
2022-08-09 10:21:14 +0200

    move files

---
+++ components/LangSwitch.vue
@@ -0,0 +1,4 @@
+<template>
+<nuxt-link class="mx-2" :to="switchLocalePath('en')">English</nuxt-link>
+<nuxt-link class="mx-2" :to="switchLocalePath('cz')">Česky</nuxt-link>
+</template>
--- components/elements/LangSwitch.vue
+++
@@ -1,4 +0,0 @@
-<template>
-<a class="mx-2 inline" href="/en">English</a>
-<a class="mx-2 inline" href="/cz">Česky</a>
-</template>
