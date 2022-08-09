019e7a4855a38da49e9728deda063ac52e2880ac
Mates Vrba <matesvrba@protonmail.com>
2022-08-09 23:42:29 +0200

    add projects to navbar, move footer

--- components/layout/navbar.vue
+++ components/layout/navbar.vue
@@ -1,10 +1,11 @@
 <template>
 		<nav class="flex lg:flex-row max-w-4xl mx-auto justify-end py-3 navbar items-center ">
 			<span class="hidden md:inline-block">
-				<a class="mx-6" href="#about">{{ $t('about') }}</a>
-				<a class="mx-6" href="#experience">{{ $t('experience')  }}</a>
-				<a class="mx-6" href="#offer">{{ $t('offer')  }}</a>
-				<a class="mx-6" href="#contact">{{ $t('contact')  }}</a>
+				<a class="mx-3" href="#about">{{ $t('about') }}</a>
+				<a class="mx-3" href="#experience">{{ $t('experience')  }}</a>
+				<a class="mx-3" href="#offer">{{ $t('offer')  }}</a>
+				<a class="mx-3" href="#projects">{{ $t('my-projects.title')  }}</a>
+				<a class="mx-3" href="#contact">{{ $t('contact')  }}</a>
 			</span>
 				<ColorSwitch class="inline"/>
 				<LangSwitch />
