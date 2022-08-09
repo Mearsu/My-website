332c8172ae1b13541802b906978918745d2ee894
Mates Vrba <matesvrba@protonmail.com>
2022-08-09 10:21:53 +0200

    add projects page

---
+++ pages/projects.vue
@@ -0,0 +1,28 @@
+<script>
+ export default {
+	 data: function(){
+		 return{
+			 cat: 'sw',
+		 }
+	 }
+ }
+</script>
+<template>
+	<div>
+		<ColorSwitch class="inline"/>
+		<LangSwitch />
+		<div class="section">
+			<div class="multiselector">
+				<div class="btn ">
+				<button  @click="cat = 'sw'">Software</button>
+				</div>
+				<div class="btn ">
+				<button @click="cat = 'hw'">Hardware</button>
+				</div>
+			</div>
+			<HwProjects v-if="cat == 'hw'"/>
+			<SwProjects v-else/>
+		</div>
+
+	</div>
+</template>
